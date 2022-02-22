485c1867bc6f0c8c4ff785624289d736802c4bfa
arsalan414 <arsalanbaba414@yahoo.com>
2022-02-22 17:24:04 +0500

    mobile version updated

---
+++ app.js
@@ -0,0 +1,8 @@
+function opennav(){
+    document.querySelector(".nav-hidden").classList.toggle("hidden")
+    // document.querySelector(".fa-xmark").classList.toggle("hidden")
+    // document.querySelector(".fa-bars-staggered").classList.toggle("hidden")
+    document.querySelector(".fa-xmark").classList.toggle("ic-hide")
+    document.querySelector(".fa-bars-staggered").classList.toggle("ic-hide")
+
+}
